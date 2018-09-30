e024baecf3f5164c70c8f6c77e8c7bdbd2dfaa9c
ARUN RAJ KUMAR <arunrkwork@gmail.com>
2018-09-30 19:02:50 +0530

    Driver and Customer Login and authentication Implemented

---
+++ app/src/main/java/com/taxi/olaclone/CustomerLoginActivity.java
@@ -0,0 +1,161 @@
+package com.taxi.olaclone;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CustomerLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static String TAG = CustomerLoginActivity.class.getSimpleName();
+
+    private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mAuth;
+    private EditText edEmail, edPass;
+    private Button btnLogin, btnRegister;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edEmail = findViewById(R.id.edEmail);
+        edPass = findViewById(R.id.edPass);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+
+                String email = edEmail.getText().toString().trim();
+                String pass = edPass.getText().toString().trim();
+
+                if (!validation(email, pass)) return;
+
+                loginAccount(email, pass);
+
+
+                break;
+            case R.id.btnRegister:
+
+                email = edEmail.getText().toString().trim();
+                pass = edPass.getText().toString().trim();
+
+                if (!validation(email, pass)) return;
+
+                createAccount(email, pass);
+
+                break;
+        }
+    }
+
+    private void createAccount(String email, String pass) {
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i(TAG, "onComplete: account create success");
+                            updateUI(mAuth.getCurrentUser());
+                            onAccountCreateSuccess();
+                        } else {
+                            Log.i(TAG, "onComplete: account create failure" + task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+
+
+    }
+
+    private void onAccountCreateSuccess() {
+        String userId = mAuth.getCurrentUser().getUid();
+        mDatabaseReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("Users")
+                .child("Customer")
+                .child(userId);
+        mDatabaseReference.setValue(true);
+    }
+
+    private void loginAccount(String email, String pass) {
+
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i(TAG, "onComplete: login success");
+                            updateUI(mAuth.getCurrentUser());
+
+                        } else {
+                            Log.i(TAG, "onComplete: login failure" + task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    private boolean validation(String email, String pass) {
+        boolean validate = true;
+        if (TextUtils.isEmpty(email)) {
+            validate = false;
+            edEmail.setError("Required");
+        } else {
+            edEmail.setError(null);
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            validate = false;
+            edPass.setError("Required");
+        } else {
+            edPass.setError(null);
+        }
+        return validate;
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+
+            String uid = currentUser.getUid();
+
+            FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(uid).getDatabase();
+
+            startActivity(new Intent(this, MapActivity.class));
+            finish();
+            return;
+        }
+    }
+}
--- app/src/main/java/com/taxi/olaclone/MainActivity.java
+++ app/src/main/java/com/taxi/olaclone/MainActivity.java
@@ -1,13 +1,37 @@
 package com.taxi.olaclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnDriver, btnCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDriver = findViewById(R.id.btnDriver);
+        btnCustomer = findViewById(R.id.btnCustomer);
+
+        btnDriver.setOnClickListener(this);
+        btnCustomer.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCustomer:
+                startActivity(new Intent(this, CustomerLoginActivity.class));
+                break;
+            case R.id.btnDriver:
+                startActivity(new Intent(this, DriverLoginActivity.class));
+                break;
+        }
     }
 }
